0ff7e94fd5f6cb936999c5afd5b558a9d8451cff
Oliul Hasnat Rafi <oliulhasnatrafi270@gmail.com>
2024-10-23 08:24:40 +0600

    ..

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,37 @@
+﻿using API_Task.Data.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Task.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DepartmentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetDept()
+        {
+            try
+            {
+                var departments = await _unitOfWork.Departments.GetAllAsync();
+
+                if (departments == null || !departments.Any())
+                {
+                    return Ok("No Departments found.");
+                }
+
+                return Ok(departments);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
